f55aff072e08bd334d1ba46f26fdb7080232b4e1
3akare <bakaredavid007@gmail.com>
2025-03-27 21:57:02 +0100

    feat: margin on site logo

--- system_implementation/client/src/components/Header.jsx
+++ system_implementation/client/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className={"container mx-auto max-w-5xl"}>
             <nav className={"w-full flex items-center justify-between h-12 py-12"}>
-                <h1 className={"text-2xl font-semibold"}> Snoopy </h1>
+                <h1 className={"text-xl lg:text-2xl font-semibold mx-4"}> Snoopy </h1>
                 <img src={avatar} alt={"avatar"} className={"size-12 rounded-full bg-white mx-4"}></img>
             </nav>
         </header>
