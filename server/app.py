e7d90166eedc9bcb5edd64f87d5f8104546d30d6
3akare <bakaredavid007@gmail.com>
2025-04-06 23:42:34 +0100

    test: updated flask application to test both lstm and nlp servers

--- server/app.py
+++ server/app.py
@@ -13,17 +13,27 @@ app.config['DEBUG'] = debug_mode
 
 CORS(app)
 
-@app.route('/', methods=['POST'])
-def hello_world():
-    # data = request.get_json()  # Ensure JSON parsing
-    # query = data.get("query", "Default message")  # Extract 'query' safely
-    # with grpc.insecure_channel("localhost:50051") as channel:
-    #     stub = sign_data_pb2_grpc.StreamDataServiceStub(channel)
-    
-    #     # Send the query as a string in RequestMessage
-    #     request_message = sign_data_pb2.RequestMessage(data=query)
-    #     response = stub.biDirectionalStream(request_message)
-    return "Server Response"
+@app.route('/nlp', methods=['POST'])
+def nlp():
+    data = request.get_json()  # Ensure JSON parsing
+    query = data.get("query", "Default message")  # Extract 'query' safely
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = sign_data_pb2_grpc.StreamDataServiceStub(channel)
+        # Send the query as a string in RequestMessage
+        request_message = sign_data_pb2.RequestMessage(data=query)
+        response = stub.biDirectionalStream(request_message)
+        return f"[nlp] {response.reply}"
+
+@app.route('/lstm', methods=['POST'])
+def lstm():
+    data = request.get_json()  # Ensure JSON parsing
+    query = data.get("query", "Default message")  # Extract 'query' safely
+    with grpc.insecure_channel("localhost:50052") as channel:
+        stub = sign_data_pb2_grpc.StreamDataServiceStub(channel)
+        # Send the query as a string in RequestMessage
+        request_message = sign_data_pb2.RequestMessage(data=query)
+        response = stub.biDirectionalStream(request_message)
+        return f"[lstm] {response.reply}"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=debug_mode, port=3000)
