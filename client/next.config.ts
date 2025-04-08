50deb18041742e77a9497cb47f96c85f05478376
3akare <bakaredavid007@gmail.com>
2025-04-08 17:53:34 +0100

    feat: installed next-pwa

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+// @ts-ignore – next-pwa is CommonJS
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
 
 const nextConfig: NextConfig = {
   output: 'export',
-};
+}
 
-export default nextConfig;
+export default withPWA(nextConfig)
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" style={{colorScheme:"dark"}}>
       <head>
+	<link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body className={`${jetbrainsMono.variable} font-mono`}>
