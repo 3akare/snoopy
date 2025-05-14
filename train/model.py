d3c70fe7e4c4b80a177414aa2ea289e7ca50afc3
3akare <bakaredavid007@gmail.com>
2025-05-14 21:24:58 +0100

    feat [train]; add drop layer in neural net to reduce overfitting

--- train/model.py
+++ train/model.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Bidirectional, LSTM, Dense
+from tensorflow.keras.layers import Bidirectional, LSTM, Dense, Dropout
 from tensorflow.keras.metrics import Precision, Recall, CategoricalAccuracy
 
 def build_model(input_shape, num_classes):
   model = Sequential([
-        Bidirectional(LSTM(64, return_sequences=True, input_shape=input_shape)),  
+        Bidirectional(LSTM(64, return_sequences=True, input_shape=input_shape)),
+        Dropout(0.2),
         Bidirectional(LSTM(32)),  
+        Dropout(0.2),
         Dense(16, activation='relu'),
+        Dropout(0.2),
         Dense(num_classes, activation='softmax')
     ])
     
