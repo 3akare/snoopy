37d1e8b559af36fa89c1035eff319d071b1b75cc
3akare <bakaredavid007@gmail.com>
2025-05-21 13:20:52 +0100

    fix [train]: fix model overfitting issues

--- train/model.py
+++ train/model.py
@@ -4,23 +4,23 @@ from tensorflow.keras.layers import Bidirectional, LSTM, Dense, Dropout
 from tensorflow.keras.metrics import Precision, Recall, CategoricalAccuracy
 
 def build_model(input_shape, num_classes):
-  model = Sequential([
+    model = Sequential([
         Bidirectional(LSTM(64, return_sequences=True, input_shape=input_shape)),
-        Dropout(0.2),
-        Bidirectional(LSTM(32)),  
-        Dropout(0.2),
-        Dense(16, activation='relu'),
-        Dropout(0.2),
+        Dropout(0.3),
+        Bidirectional(LSTM(32)),
+        Dropout(0.3),
+        Dense(32, activation='relu'),
+        Dropout(0.3),
         Dense(num_classes, activation='softmax')
     ])
     
-  model.compile(
-      optimizer='adam',
-      loss='categorical_crossentropy', 
-      metrics=[
-          CategoricalAccuracy(name='accuracy'),
-          Precision(name='precision'),
-          Recall(name='recall')
-      ]
-  )
-  return model
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=[
+            CategoricalAccuracy(name='accuracy'),
+            Precision(name='precision'),
+            Recall(name='recall')
+        ]
+    )
+    return model
