f9a464c6fe31084827d4476646a3294d3b7dccf6
3akare <bakaredavid007@gmail.com>
2025-06-19 20:29:29 +0100

    feat [train]: add utility functions for configuration management and sequence padding

--- train/train_utils.py
+++ train/train_utils.py
@@ -18,15 +18,6 @@ def load_config(filepath):
 def pad_or_truncate_sequence(sequence: np.ndarray, target_length: int, padding_value: float = 0.0) -> np.ndarray:
     """
     Pads or truncates a NumPy sequence to a target length.
-
-    Args:
-        sequence (np.ndarray): The input NumPy array representing a sequence of keypoints.
-                               Shape: (num_frames, feature_dim).
-        target_length (int): The desired length of the sequence.
-        padding_value (float): The value to use for padding.
-
-    Returns:
-        np.ndarray: The padded or truncated sequence.
     """
     if sequence.shape[0] > target_length:
         return sequence[:target_length, :]
